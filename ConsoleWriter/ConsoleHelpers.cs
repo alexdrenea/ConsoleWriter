ff3c9d2982bfbd464ee81d0bc0feef0349ab897a
Alex Drenea <alexdrenea@hotmail.com>
2020-05-30 18:04:30 -0400

    initial commit

---
+++ ConsoleWriter/ConsoleHelpers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleWriter
+{
+    public static class ConsoleHelpers
+    {
+        public static void WriteLine<T>(this T entity, ConsoleColor? color = null)
+        {
+            if (color.HasValue)
+                Console.ForegroundColor = color.Value;
+            Console.WriteLine(entity != null ? Newtonsoft.Json.JsonConvert.SerializeObject(entity, Newtonsoft.Json.Formatting.Indented) : "null");
+            Console.ResetColor();
+        }
+
+        public static void WriteLine(this string message, ConsoleColor? color = null)
+        {
+            if (color.HasValue)
+                Console.ForegroundColor = color.Value;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public static void WriteInLine(this string message, ConsoleColor? color = null)
+        {
+            if (color.HasValue)
+                Console.ForegroundColor = color.Value;
+            Console.Write(message); //make sure there's a space at the end of the message since it's inline.
+            Console.ResetColor();
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Console Writer
+
+The `ConsoleWriter` library exposes some Console.Write extensions that allow you to have a nicer looking output from your methods by using colors (There are libraries out there that specialize in this and you can definitely use one of those, but if all you require is simple color you can certanly stick with what's offered here')
