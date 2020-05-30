5fc1482e8d1dae6549eb9e481501aeafdb276b82
Alex Drenea <alexdrenea@hotmail.com>
2020-05-30 19:39:44 -0400

    update project with nuget info
    first couple lines in the test app.

--- test/ConsoleWriter.Test/Program.cs
+++ test/ConsoleWriter.Test/Program.cs
@@ -8,6 +8,12 @@ namespace ConsoleWriter.Test
         static void Main(string[] args)
         {
             ConsoleHelpers.WriteLine("Hello World!");
+
+            ConsoleHelpers.WriteLine("Hello World! in Yellow", ConsoleColor.DarkYellow);
+
+            ConsoleHelpers.WriteInLine("Hello World! partially ");
+            ConsoleHelpers.WriteLine("in Yellow", ConsoleColor.DarkYellow);
+
         }
     }
 }
